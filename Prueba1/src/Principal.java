b385da9bc1b95fdb2e2ca4987aa9f8fa2b4e2c0c
alu1daw <alu1daw@DESKTOP-HRL81S7>
2021-10-08 13:59:25 +0200

    Segunda validacion

--- Prueba1/src/Principal.java
+++ Prueba1/src/Principal.java
@@ -3,7 +3,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola mundo de java");
+		System.out.println("Hola mundo de java2");
 	}
 
 }
